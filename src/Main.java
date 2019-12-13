ae1548373f323dd82396cfd4e541f612dfd75a18
Ana Mari Noel <anamari.noel@finbet.com>
2019-12-13 12:16:40 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        ArrayList<Double> myDoubleList = new ArrayList<Double>();
+//
+//        for (double dbl=0.1; dbl <= 10.0; dbl+= 0.5) {
+//            myDoubleList.add(Double.valueOf(dbl)); // myDoublList.add(dbl);
+//        }
+//
+//        for (int i=0; i<myDoubleList.size(); i++) {
+//            double value = myDoubleList.get(i).doubleValue(); // double value = myDoublList.get(i);
+//            System.out.println(i + "-->" + value);
+//        }
+    }
+}
